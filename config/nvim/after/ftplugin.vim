b12ebaf56ddf68707f3bfab632338f13e93931f1
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-01-03 23:49:42 +0900

    ftplugin.vim を追加

---
+++ config/nvim/after/ftplugin.vim
@@ -0,0 +1,2 @@
+" コメントで改行する際､コメントアウトをしないで改行する
+au FileType * set fo-=c fo-=r fo-=o
--- symbolic.sh
+++ symbolic.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/.profile ~/.profile
 
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/config/nvim/after/ftplugin.vim ~/.config/nvim/after/ftplugin.vim
