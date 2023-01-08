10d6709821e32a5b6dbab300a15ad071f0df8f16
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-01-08 16:07:06 +0900

    feat: gitconfig を dotfiles で管理

--- symbolic.sh
+++ symbolic.sh
@@ -1,9 +1,13 @@
 #!bin/bash
 
+# git
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
 
+# shell
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.profile ~/.profile
 
+# vim
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/config/nvim/after/ftplugin.vim ~/.config/nvim/after/ftplugin.vim
