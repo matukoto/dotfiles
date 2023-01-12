012b558eb03846b339404bb8d9a9e9ac3083932f
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-01-12 19:08:20 +0900

    feat: md 編集時に箇条書き、チェックリストを改行したとき、自動で継続されるように変更

--- config/nvim/after/ftplugin.vim
+++ config/nvim/after/ftplugin.vim
@@ -1,2 +1,3 @@
 " コメントで改行する際､コメントアウトをしないで改行する
-au FileType * set fo-=c fo-=r fo-=o
+au FileType * set fo-=cro
+au FileType markdown setlocal fo+=jro
--- symbolic.sh
+++ symbolic.sh
@@ -11,4 +11,5 @@ ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/config/nvim/after/ftplugin.vim ~/.config/nvim/after/ftplugin.vim
+ln -s ~/dotfiles/config/nvim/after/ftplugin/ ~/.config/nvim/after/
 ln -s ~/dotfiles/vim/coc/coc-settings.json ~/.config/nvim/coc-settings.json
