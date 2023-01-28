feff079fdbd3f9cd44ddcbaf7f9d75d1d458aa4e
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-01-28 22:20:37 +0900

    feat: wezterm 利用開始

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 return {
-  default_prog = {"wsl.exe"},
-  color_scheme = "Edpresso",
-  font = wezterm.font("UDEV Gothic 35NF"),
+  default_prog = {"wsl.exe", "--cd", "~"},
+  color_scheme = "Edge Dark (base16)",
+  font = wezterm.font("UDEV Gothic 35NFLG"),
   font_size = 12.0, -- フォントサイズは偶数でないと変になる
 }
