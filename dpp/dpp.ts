8e6079741954c904e23e85e7d7b8819f5436b9f6
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-01-21 21:13:15 +0900

    feat: dpp.ts OK コピペ

--- dpp/dpp.ts
+++ dpp/dpp.ts
@@ -5,3 +5,110 @@ import {
   Plugin,
 } from "https://deno.land/x/dpp_vim@v0.0.5/types.ts";
 import { Denops, fn } from "https://deno.land/x/dpp_vim@v0.0.5/deps.ts";
+
+export class Config extends BaseConfig {
+  override async config(args: {
+    denops: Denops;
+    contextBuilder: ContextBuilder;
+    basePath: string;
+    dpp: Dpp;
+  }): Promise<{
+    Plugin: Plugin[];
+    stateLines: stringp[];
+  }> {
+    args.contextBuilder.setGlobal({
+      protocols: ["git"],
+    });
+
+    type Toml = {
+      hooks_file?: string;
+      ftplugins?: Record<string, string>;
+      plugins?: Plugin[];
+    };
+
+    type LazyMakeStateResult = {
+      plugins: Plugin[];
+      stateLines: string[];
+    };
+
+    const [context, options] = await arggs.contextBuilder.get(args.denops);
+    const dotfilesDir = "~/.config/nvim/dpp/";
+
+    const tomls = Toml[] = [];
+    tomls.push(
+      await args.dpp.extAction(
+        args.denops,
+        context,
+        options,
+        "toml",
+        "load",
+        {
+          path: await fn.expand(args.denops, dotfilesDir + "dein.toml"),
+          options: {
+            lazy: false,
+          },
+        },
+      ) as Toml,
+    );
+
+    tomls.push(
+      await args.dpp.extAction(
+        args.denops,
+        context,
+        options,
+        "toml",
+        "load",
+        {
+          path: await fn.expand(args.denops, dotfilesDir + "dein_lazy.toml"),
+          options: {
+            lazy: true,
+          },
+        },
+      ) as Toml,
+    );
+
+    const recordPlugins: Record<string, Plugin> = {};
+    const ftplugins: Record<string, string> = {};
+    const hooksFile: string[] = [];
+
+    toml.forEach((toml) => {
+
+      for (const plugin of toml.plugins) {
+        recordPlugins[plugin.name] = plugin;
+      }
+
+      if (toml.ftplugins) {
+        for (const filetype of Object.keys(toml.ftplugins)) {
+          if (ftplugins[filetype]) {
+            ftplugins[filetype] += '\n${toml.ftplugins[filetype]}';
+          } else {
+            ftplugins[filetype] = toml.ftplugins[filetype];
+          }
+        }
+      }
+
+      if (toml.hooks_file) {
+        hooksFile.push(toml.hooks_file);
+      }
+
+    });
+
+    const lazyResult = await args.dpp.extAction(
+      args.denops,
+      context,
+      options,
+      "lazy",
+      "makeState",
+      {
+        plugins: Object.values(recordPlugins),
+      },
+    ) as LazyMakeStateResult;
+
+    console.log(lazyResult);
+
+    return {
+      plugins: lazyResult.plugins,
+      stateLines: lazyResult.stateLines,
+    };
+  }
+}
