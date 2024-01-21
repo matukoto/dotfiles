32dfda9e33e277fa2889c2d6a737c439475612b0
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-01-21 20:45:50 +0900

    feat: dpp.ts init

---
+++ dpp/dpp.ts
@@ -0,0 +1,7 @@
+import {
+  BaseConfig,
+  ContextBuilder,
+  Dpp,
+  Plugin,
+} from "https://deno.land/x/dpp_vim@v0.0.5/types.ts";
+import { Denops, fn } from "https://deno.land/x/dpp_vim@v0.0.5/deps.ts";
