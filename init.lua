f1bce9b24d8d12103387f04d2417649447e79130
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-02-05 22:23:43 +0900

    feat: comment out obsidian setup

--- init.lua
+++ init.lua
@@ -37,6 +37,9 @@ vim.keymap.set("n", "<leader>s", "<cmd>Telescope frecency<CR>")
 vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<CR>")
 vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>")
 
+-- obsidian
+-- require('obsidian').setup()
+
 -- lualine
 require('lualine').setup()
 
