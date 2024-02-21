f459bc34ee9927a4b7b05a764dcc0c0749079862
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-02-21 22:28:48 +0900

    feat: wezterm setting fix

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,16 @@
 local wezterm = require 'wezterm'
-return {
-  window_background_opacity = 0.85, -- 透過率
-  --default_prog = {"wsl.exe", "--cd", "~"},
-  color_scheme = "Edge Dark (base16)",
-  font = wezterm.font("PlemolJP Console NF"),
-  font_size = 12.0, -- フォントサイズは偶数でないと変になる
-  initial_cols = 120,
-  initial_rows = 37,
-  -- keybind ファイル読み込み
-  keys = require("keybinds").keys,
-  key_tables = require("keybinds").key_tables,
-  use_ime = false,
-}
+
+local config = {}
+
+config.window_background_opacity = 0.85 -- 透過率
+config.color_scheme = 'Edge Dark (base16)'
+config.font = wezterm.font("PlemolJP Console NF")
+config.font_size = 12.0 -- フォントサイズは偶数でないと変にな
+config.initial_cols = 120
+config.initial_rows = 37
+--config.-- keybind ファイル読み込み
+config.keys = require("keybinds").keys
+config.key_tables = require("keybinds").key_tables
+config.use_ime = false
+
+return config
