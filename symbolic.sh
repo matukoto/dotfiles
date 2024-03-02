7488707b61915906464c73e20ca760410eb86f59
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-03-03 07:28:54 +0900

    feat: mkdir -p

--- symbolic.sh
+++ symbolic.sh
@@ -13,16 +13,20 @@ ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 
 # vim
+mkdir -p ~/.config/nvim/lua
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
 unlink ~/.config/nvim/init.lua
 ln -s ~/dotfiles/init.lua ~/.config/nvim/lua/init.lua
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+mkdir -p ~/.config/nvim/after
 ln -s ~/dotfiles/config/nvim/after/ftplugin.vim ~/.config/nvim/after/ftplugin.vim
 ln -s ~/dotfiles/config/nvim/after/ftplugin/ ~/.config/nvim/after/
 ln -s ~/dotfiles/vim/coc/coc-settings.json ~/.config/nvim/coc-settings.json
+mkdir -p ~/.config/nvim/plugin
 ln -s ~/dotfiles/vim/gin-preview.vim ~/.config/nvim/plugin/gin-preview.vim
 
 # skk
+mkdir ~/.skk
 ln -s ~/dotfiles/.skk/SKK-JISYO.L ~/.skk/SKK-JISYO.L
 ln -s ~/dotfiles/.skk/SKK-JISYO.jinmei ~/.skk/SKK-JISYO.jinmei
 ln -s ~/dotfiles/.skk/SKK-JISYO.geo ~/.skk/SKK-JISYO.geo
@@ -30,13 +34,15 @@ ln -s ~/dotfiles/.skk/SKK-JISYO.propernoun ~/.skk/SKK-JISYO.propernoun
 ln -s ~/dotfiles/.skk/SKK-JISYO.emoji.utf8 ~/.skk/SKK-JISYO.emoji.utf8
 
 # textlint
-ln -s ~/dotfiles/.textlintrc ~/.textlintrc
-ln -s ~/dotfiles/config/efm-langserver/config.yaml ~/.config/efm-langserver/config.yaml
+# ln -s ~/dotfiles/.textlintrc ~/.textlintrc
+# ln -s ~/dotfiles/config/efm-langserver/config.yaml ~/.config/efm-langserver/config.yaml
 
 # yazi
+mkdir -p ~/.config/yazi
 ln -s ~/dotfiles/config/yazi ~/.config/
 
 ## starship
+
 ln -s ~/dotfiles/config/starship.toml ~/.config/starship.toml
 
 # gitui
