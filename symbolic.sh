1e2d48e5881873d4c6963436183ff0e9e5a7fc46
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-03-12 23:41:48 +0900

    feat: unlink

--- symbolic.sh
+++ symbolic.sh
@@ -15,7 +15,6 @@ ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 # vim
 mkdir -p ~/.config/nvim/lua
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
-unlink ~/.config/nvim/init.lua
 ln -s ~/dotfiles/init.lua ~/.config/nvim/lua/init.lua
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 mkdir -p ~/.config/nvim/after
@@ -45,14 +44,41 @@ ln -s ~/dotfiles/.skk/SKK-JISYO.emoji.utf8 ~/.skk/SKK-JISYO.emoji.utf8
 mkdir -p ~/.config/yazi
 ln -s ~/dotfiles/config/yazi/theme.toml ~/.config/yazi/theme.toml
 ln -s ~/dotfiles/config/yazi/yazi.toml ~/.config/yazi/yazi.toml
-
 ## starship
 
 ln -s ~/dotfiles/config/starship.toml ~/.config/starship.toml
-
 # gitui
 ln -s ~/dotfiles/config/gitui/key_bindings.ron ~/.config/gitui/key_bindings.ron
-
 # wezterm
 ln -s ~/dotfiles/wezterm/wezterm.lua ~/.wezterm.lua
 ln -s ~/dotfiles/wezterm/keybinds.lua ~/.config/wezterm/keybinds.lua
+# unlink
+unlink ~/.gitconfig
+unlink ~/.gitinore_global
+unlink ~/.config/jj/config.toml
+unlink ~/.bashrc
+unlink ~/.bash_aliases
+unlink ~/.profile
+unlink ~/.vimrc
+unlink ~/.config/nvim/init.vim
+unlink ~/.config/nvim/lua/init.lua
+unlink ~/.config/nvim/init.lua
+unlink ~/.config/nvim/after/ftplugin.vim
+unlink ~/.config/nvim/after/ftplugin/
+unlink ~/.config/nvim/coc-settings.json
+
+unlink ~/.skk/SKK-JISYO.L
+unlink ~/.skk/SKK-JISYO.jinmei
+unlink ~/.skk/SKK-JISYO.geo
+unlink ~/.skk/SKK-JISYO.propernoun
+unlink ~/.skk/SKK-JISYO.emoji.utf8
+
+unlink ~/.config/nvim/plugin/gin-preview.vim
+unlink ~/.vim/dadbod-ui/connections.json
+unlink ~/.config/sqls/config.yaml
+unlink ~/.config/yazi/yazi.toml
+unlink ~/.config/yazi/theme.toml
+unlink ~/.config/starship.toml
+unlink ~/.config/gitui/key_bindings.ron
+unlink ~/.wezterm.lua
+unlink ~/.config/wezterm/keybinds.lua
