152d1335fcfddc46603290be7eccdc9bfda0877d
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-04-17 08:44:08 +0900

    feat: add lualine config

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -101,7 +101,46 @@ vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>")
 require('fidget').setup()
 
 -- lualine
-require('lualine').setup()
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
 
 -- comment.nvim
 require('Comment').setup(
@@ -183,7 +222,7 @@ require("hlchunk").setup({
     enable = false
   },
   line_num = {
-    enable =false
+    enable = false
   },
   blank = {
     enable = false
