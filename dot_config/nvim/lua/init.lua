90cb33f1942396781ff8ad881e7088394280dd27
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-04-17 21:34:48 +0900

    feat: add lualine

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -136,9 +136,29 @@ require('lualine').setup {
         }
       }
     },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_x = { 'searchcount' },
+    --{
+    --   'diagnostics',
+    --   sources = {
+    --     -- 'nvim_diagnostic',
+    --     'nvim_lsp',
+    --   },
+    --
+    --   sections = { 'error', 'warn', 'info', 'hint' },
+    --
+    --   diagnostics_color = {
+    --     error = 'DiagnosticError',
+    --     warn  = 'DiagnosticWarn',
+    --     info  = 'DiagnosticInfo',
+    --     hint  = 'DiagnosticHint',
+    --   },
+    --   symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
+    --   colored = true,
+    --   update_in_insert = false,
+    --   always_visible = false,
+    -- },
+    lualine_y = { 'encoding', 'fileformat', 'filetype' },
+    lualine_z = { 'location', 'progress' }
   },
   inactive_sections = {
     lualine_a = {},
