fbf2ed03217ac41b9f84b01fb8cc771f0ed81f72
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-04-26 17:45:21 +0900

    feat: add chowcho.nvim setting

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -505,6 +505,43 @@ vim.keymap.set('n', '*', function()
   end
 end, { silent = true, expr = true })
 
+require('chowcho').setup {
+  -- Must be a single character. The length of the array is the maximum number of windows that can be moved.
+  labels = { "A", "B", "C", "D", "E", "F", "G", "H", "I" },
+  use_exclude_default = true,
+  ignore_case = true,
+  exclude = function(buf, win)
+    -- exclude noice.nvim's cmdline_popup
+    local bt = vim.api.nvim_get_option_value("buftype", { buf = buf })
+    local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
+    if bt == "nofile" and (ft == "noice" or ft == "vim") then
+      return true
+    end
+    return false
+  end,
+  selector = {
+    float = {
+      border_style = "rounded",
+      icon_enabled = true,
+      color = {
+        label = {
+          active = "#c8cfff",
+          inactive = "#ababab",
+        },
+        text = {
+          active = "#fefefe",
+          inactive = "#d0d0d0",
+        },
+        border = {
+          active = "#b400c8",
+          inactive = "#fefefe",
+        },
+      },
+      zindex = 1,
+    },
+  },
+}
+
 -- treesitter
 require('nvim-treesitter.configs').setup {
   ensure_installed = {
