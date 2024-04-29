dc7c2d585f1eaa348c9d456474e2f567af5354b9
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-04-29 21:23:06 +0900

    feat: lua plugin file separate

---
+++ dot_config/nvim/plugin.d/nvim-treesitter.lua
@@ -0,0 +1,24 @@
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    "typescript",
+    "tsx",
+    "javascript",
+    "vue",
+    "vim",
+    "svelte",
+    "json",
+    "markdown",
+    "lua",
+    "html",
+    "css",
+    "java",
+    --"yaml",
+    "go",
+  },
+  highlight = {
+    enable = true,
+  },
+  endwise = {
+    enable = true,
+  },
+}
