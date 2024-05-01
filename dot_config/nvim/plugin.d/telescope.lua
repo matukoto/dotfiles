f79acafe3b03d2f28c729f84dde6294779ac886b
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-05-01 12:24:56 +0900

    feat: add fzy_native extension to telescope

--- dot_config/nvim/plugin.d/telescope.lua
+++ dot_config/nvim/plugin.d/telescope.lua
@@ -16,13 +16,13 @@ require("telescope").setup {
       prompt_position = "top",
       preview_cutoff = 1,
     },
+    extensions = {
+      fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true,
+      },
+    }
   },
-  extensions = {
-    -- fzy_native = {
-    -- override_generic_sorter = false,
-    -- override_file_sorter = true,
-    -- },
-  }
 }
 -- require('telescope').load_extension('fzy_native')
 
