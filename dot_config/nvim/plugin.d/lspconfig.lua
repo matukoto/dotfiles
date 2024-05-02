e68e8501d8c5d7dbef01e0dbaae887ad379d8d5d
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-05-02 18:18:10 +0900

    feat: add lua-language-server settings for lua plugin.

--- dot_config/nvim/plugin.d/lspconfig.lua
+++ dot_config/nvim/plugin.d/lspconfig.lua
@@ -20,3 +20,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { buffer = true })
   end,
 })
+local lspconfig = require('lspconfig')
+
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      -- runtime = {
+      --   version = "LuaJIT",
+      --   pathStrict = true,
+      --   path = { "?.lua", "?/init.lua" },
+      -- },
+      workspace = {
+        library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
+          "${3rd}/luv/library",
+          "${3rd}/busted/library",
+          "${3rd}/luassert/library",
+        }),
+        checkThirdParty = "Disable",
+      },
+    },
+  },
+})
