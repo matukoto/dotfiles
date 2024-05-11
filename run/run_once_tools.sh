0be414c9b890739253b1aa3b4ccc6d05bf7cd83b
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-05-11 17:01:16 +0900

    feat: add nushell and starship init

--- run/run_once_tools.sh
+++ run/run_once_tools.sh
@@ -5,7 +5,17 @@
 
 # mise
 curl https://mise.run | sh
+mise i
 
+# nu starship
+git clone https://github.com/nushell/nushell.git
+cd nushell
+cargo build --release --workspace; cargo run --release
+
+curl -sS https://starship.rs/install.sh | sh
+mkdir ~/.cache/starship
+starship init nu | save -f ~/.cache/starship/init.nu
+use ~/.cache/starship/init.nu
 # yay
 # yay -S --noconfirm sqlite
 # yay -S --noconfirm jq
