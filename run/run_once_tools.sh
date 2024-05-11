5240fa5f083f69f7aa912962b3f6008bca5e8c17
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-05-11 17:07:36 +0900

    feat: add rustup

--- run/run_once_tools.sh
+++ run/run_once_tools.sh
@@ -3,6 +3,9 @@
 # run_once_hogehoge は `chezmoi apply` した際に以前に実行されたことがなければ実行されるファイル
 # 主にツールのインストールなど、一度だけ実行すれば良いものを記述する
 
+# rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 # mise
 curl https://mise.run | sh
 mise i
