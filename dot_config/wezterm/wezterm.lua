b255e423e0c95f33b898e3e56084f1909d9f8ddf
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-05-11 12:54:02 +0900

    feat: wezterm font

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require('wezterm')
 local config = {
   window_background_opacity = 0.85, -- 透過率
+  default_prog = { 'bash' },
   --default_prog = {"wsl.exe", "--cd", "~"},
   color_scheme = 'Edge Dark (base16)',
-  font = wezterm.font('PlemolJP Console NF'),
-  font_size = 12.0, -- フォントサイズは偶数でないと変になる
+  font = wezterm.font('PlemolJP35 Console NF'),
+  font_size = 14.0, -- フォントサイズは偶数でないと変になる
   initial_cols = 120,
   initial_rows = 37,
   -- keybind ファイル読み込み
