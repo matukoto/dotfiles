5695db2fbc4c01d044d49011912f379a1d600b2d
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-05-12 14:02:58 +0900

    feat: hslens add keymap

--- dot_config/nvim/plugin.d/hlslens.lua
+++ dot_config/nvim/plugin.d/hlslens.lua
@@ -1,1 +1,14 @@
 require('hlslens').setup()
+
+vim.api.nvim_set_keymap(
+  'n',
+  '*',
+  [[*<Cmd>lua require('hlslens').start()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  'n',
+  '#',
+  [[#<Cmd>lua require('hlslens').start()<CR>]],
+  { noremap = true, silent = true }
+)
