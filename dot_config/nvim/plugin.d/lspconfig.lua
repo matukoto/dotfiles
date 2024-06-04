34d91e3006ca1f00189a28f2d2003482a40244b1
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-06-04 22:31:10 +0900

    feat: add capabilities to lspconfig setup

--- dot_config/nvim/plugin.d/lspconfig.lua
+++ dot_config/nvim/plugin.d/lspconfig.lua
@@ -21,7 +21,9 @@ require('ddc_source_lsp_setup').setup()
 -- local capabilities = require('ddc_source_lsp').make_client_capabilities()
 -- require('mason-lspconfig').setup_handlers({
 --   function(server_name)
---     require('lspconfig')[server_name].setup({})
+--     require('lspconfig')[server_name].setup({
+--       capabilities = capabilities,
+--     })
 --   end,
 -- })
 
