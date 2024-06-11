47824816eb3591b87ecdaa7e395e5e2c7fa64a91
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-06-11 23:06:29 +0900

    feat: require.lua  にまとめた

---
+++ dot_config/nvim/plugin.d/require.lua
@@ -0,0 +1,19 @@
+-- require など簡単な設定をまとめたファイル
+
+-- キーストロークを表示する
+require('keycastr').setup()
+
+-- LSP の諸々を表示する
+require('fidget').setup()
+
+-- visual mode で行全体をマ-キングする
+require('full_visual_line').setup()
+
+-- scroll bar を表示する
+require('scrollbar').setup()
+
+-- markdown をレンダリングする
+require('render-markdown').setup()
+
+-- カーソル移動を可視化する
+require('smoothcursor').setup()
