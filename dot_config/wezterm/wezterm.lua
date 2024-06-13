4bd7b783e82e25ef4bf552c22361d0c801e14d61
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-06-14 08:45:33 +0900

    feat: fix wezterm default_prog

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require('wezterm')
 local config = {
   window_background_opacity = 0.85, -- 透過率
-  default_prog = { 'bash' },
-  --default_prog = {"wsl.exe", "--cd", "~"},
+  -- default_prog = { 'bash' },
+  default_prog = { 'wsl.exe', '--cd', '~' },
   color_scheme = 'Edge Dark (base16)',
   font = wezterm.font('PlemolJP35 Console NF'),
   font_size = 14.0, -- フォントサイズは偶数でないと変になる
@@ -13,4 +13,5 @@ local config = {
   key_tables = require('keybinds').key_tables,
   use_ime = false,
 }
+
 return config
