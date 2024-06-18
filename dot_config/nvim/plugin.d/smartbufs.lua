70c7bba26c1de5ae7d74865d15e984164f90c35c
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-06-18 22:26:53 +0900

    feat: change keymap for smartbufs <C-i> and <C-o> to <C-l> and <C-h>

--- dot_config/nvim/plugin.d/smartbufs.lua
+++ dot_config/nvim/plugin.d/smartbufs.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '<C-i>', '<cmd>lua require("nvim-smartbufs").goto_next_buffer()<CR>')
-vim.keymap.set('n', '<C-o>', '<cmd>lua require("nvim-smartbufs").goto_prev_buffer()<CR>')
+vim.keymap.set('n', '<C-l>', '<cmd>lua require("nvim-smartbufs").goto_next_buffer()<CR>')
+vim.keymap.set('n', '<C-h>', '<cmd>lua require("nvim-smartbufs").goto_prev_buffer()<CR>')
