867bebf4a69921f131a9882457b04c16dac3caff
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-06-23 11:09:02 +0900

    feat: add keymap <C-w> to delete buffer

--- dot_config/nvim/plugin.d/smartbufs.lua
+++ dot_config/nvim/plugin.d/smartbufs.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<C-l>', '<cmd>lua require("nvim-smartbufs").goto_next_buffer()<CR>')
 vim.keymap.set('n', '<C-h>', '<cmd>lua require("nvim-smartbufs").goto_prev_buffer()<CR>')
+vim.keymap.set('n', '<C-w>', '<cmd>bdelete<CR>', { nowait = true })
