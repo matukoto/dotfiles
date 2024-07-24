48dc0d91693c31a94527c3f636376cabcc072172
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-07-24 18:37:14 +0900

    fix: fish pull_skk_dict: git pull -> git rebase

--- dot_config/fish/functions/pull_skk_dict.fish
+++ dot_config/fish/functions/pull_skk_dict.fish
@@ -5,11 +5,12 @@ function pull_skk_dict
     # skk ディレクトリへ移動
     cd ~/.skk
 
-    # 現在の日付を取得 (YYYY-MM-DD 形式)
+    # リモートリポジトリの更新
+    git fetch
 
     # Git 操作の実行
-    if not git pull --quiet
-        echo "エラー: git pullに失敗しました。" >&2
+    if not git rebase origin/main
+        echo "エラー: git rebaseに失敗しました。" >&2
         cd $current_dir
         return 1
     end
