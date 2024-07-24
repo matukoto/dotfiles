216de30ca0a27c8828bdb1a61f4cf3246c22a484
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-07-24 18:35:16 +0900

    feat: add rainbow-csv setting

---
+++ dot_config/nvim/plugin.d/rainbow-csv.lua
@@ -0,0 +1,11 @@
+vim.g.rcsv_colorpairs = {
+  { 'red', 'red' },
+  { 'yellow', 'yellow' },
+  { 'lightgray', 'lightgray' },
+  { 'lightgreen', 'lightgreen' },
+  { 'lightblue', 'lightblue' },
+  { 'cyan', 'cyan' },
+  { 'lightred', 'lightred' },
+  { 'darkyellow', 'darkyellow' },
+  { 'white', 'white' },
+}
