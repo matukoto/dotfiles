840ee8c1f236ab8179e5632179a42f69b783fbff
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-07-25 02:57:00 +0000

    feat: add javatest keymap

---
+++ dot_config/nvim/after/ftplugin/java.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', 'gTM', '<cmd>JavaTestRunCurrentMethod<CR>')
+vim.keymap.set('n', 'gTC', '<cmd>JavaTestRunCurrentClass<CR>')
+vim.keymap.set('n', 'gTV', '<cmd>JavaTestViewLastReport<CR>')
