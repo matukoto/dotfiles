7ed74b58bef7a9b39a3b3e6f0646b4d61a5ecbaf
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-08-06 22:23:27 +0900

    feat: conform.nvim add stop_after_first option to formatters

--- dot_config/nvim/plugin.d/conform.lua
+++ dot_config/nvim/plugin.d/conform.lua
@@ -1,22 +1,23 @@
-local jsFormatter = { { 'biome', 'prettierd', 'prettier' } }
+local jsFormatter = { { 'biome', 'prettierd', 'prettier', stop_after_first = true } }
 
 require('conform').setup({
   formatters_by_ft = {
     lua = { 'stylua' }, -- 特に設定しなくても ~/.config/stylua/stylua.toml を見てくれる
     sql = { 'sql_formatter' },
-    java = {},
-    sh = { 'shellcheck', 'shfmt' },
+    java = { lsp_format = 'fallback' },
+    sh = { 'shellcheck', 'shfmt', stop_after_first = false },
     json = { 'jq' },
     javascript = jsFormatter,
     javascriptreact = jsFormatter,
     typescript = jsFormatter,
     typescriptreact = jsFormatter,
     -- ['*'] = { 'typos' }, -- 勝手に訂正されてしまうので、状況次第では有用
-    markdown = { 'typos', 'markdownlint-cli2' },
+    markdown = { 'typos', 'markdownlint-cli2', stop_after_first = false },
   },
   format_on_save = {
     timeout_ms = 1000,
     lsp_fallback = true, -- LSP のフォーマッタを使う
     quiet = false,
+    stop_after_first = true,
   },
 })
