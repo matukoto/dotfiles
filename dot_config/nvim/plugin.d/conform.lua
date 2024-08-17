0c3a53c91a60ea1d773261a7f15271fbc6118bc5
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-08-17 12:00:15 +0900

    feat: conform.nvim stop_after_first

--- dot_config/nvim/plugin.d/conform.lua
+++ dot_config/nvim/plugin.d/conform.lua
@@ -1,23 +1,23 @@
-local jsFormatter = { { 'biome', 'prettierd', 'prettier', stop_after_first = true } }
+local jsFormatter = { { 'biome', 'prettierd', 'prettier' } }
 
 require('conform').setup({
   formatters_by_ft = {
     lua = { 'stylua' }, -- 特に設定しなくても ~/.config/stylua/stylua.toml を見てくれる
     sql = { 'sql_formatter' },
     java = { lsp_format = 'fallback' },
-    sh = { 'shellcheck', 'shfmt', stop_after_first = false },
+    sh = { 'shellcheck', 'shfmt' },
     json = { 'jq' },
     javascript = jsFormatter,
     javascriptreact = jsFormatter,
     typescript = jsFormatter,
     typescriptreact = jsFormatter,
     -- ['*'] = { 'typos' }, -- 勝手に訂正されてしまうので、状況次第では有用
-    markdown = { 'typos', 'markdownlint-cli2', stop_after_first = false },
+    markdown = { 'typos', 'markdownlint-cli2' },
   },
   format_on_save = {
     timeout_ms = 1000,
     lsp_fallback = true, -- LSP のフォーマッタを使う
     quiet = false,
-    stop_after_first = true,
+    stop_after_first = false,
   },
 })
