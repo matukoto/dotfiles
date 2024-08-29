5dc3ab7384ac473eb4fd39768473cae5c360dfdb
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-08-29 21:58:19 +0900

    feat: add aqua

--- run/run_once_tools.sh
+++ run/run_once_tools.sh
@@ -9,6 +9,12 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # chezmoi
 sh -c "$(curl -fsLS get.chezmoi.io)"
 
+# aqua
+curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
+echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  aqua-installer" | sha256sum -c
+chmod +x aqua-installer
+./aqua-installer
+
 # mise
 curl https://mise.run | sh
 mise i
