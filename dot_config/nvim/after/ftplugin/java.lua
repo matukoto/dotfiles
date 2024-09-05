2c1d3d6fa25ac0837a029f1f7dfebe6cd0402d0a
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-09-05 20:43:43 +0900

    feat: java keymap

--- dot_config/nvim/after/ftplugin/java.lua
+++ dot_config/nvim/after/ftplugin/java.lua
@@ -1,6 +1,3 @@
 vim.keymap.set('n', 'gTM', ":lua require('java').test.run_current_method()<CR>", { silent = true })
 vim.keymap.set('n', 'gTC', ":lua require('java').test.run_current_class()<CR>", { silent = true })
-vim.keymap.set('n', 'gTV', function()
-  vim.cmd([[ vsplit ]])
-  require('java').test.view_last_report()
-end, { silent = true })
+vim.keymap.set('n', 'gTV', ":lua require('java').test.view_last_report()<CR>", { silent = true })
