7881f42aa60b17fca0547d31e6f60bad3b4fbf11
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-09-15 12:12:55 +0900

    feat: wezterm columns 120 -> 110

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = {
   color_scheme = 'Edge Dark (base16)',
   font = wezterm.font('PlemolJP35 Console NF'),
   font_size = 14.0, -- フォントサイズは偶数でないと変になる
-  initial_cols = 120,
+  initial_cols = 110,
   initial_rows = 37,
   -- keybind ファイル読み込み
   keys = require('keybinds').keys,
