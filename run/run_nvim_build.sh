f6ab3a466949fbf86016893ced2adcfbbb914a62
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-09-25 20:30:20 +0900

    fix: neovim build script

--- run/run_nvim_build.sh
+++ run/run_nvim_build.sh
@@ -7,6 +7,8 @@ cd "${HOME}"/work/github.com/neovim/neovim || exit
 git fetch --prune origin
 git rebase origin/master
 
+make distclean
+
 make CMAKE_BUILD_TYPE=Release \
   CMAKE_INSTALL_PREFIX="$HOME"/.local/ \
   BUNDLED_CMAKE_FLAG='-DUSE_BUNDLED_TS_PARSERS=OFF'
