aaf1a00bd097243955b44ee787e3de58f37e5d4d
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-10-01 20:27:55 +0900

    style: "diff"

--- run/run_once_sudo.sh
+++ run/run_once_sudo.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 # Vim で使う
-sudo apt install libncurses5-dev libncursesw5-dev make cmake ninja-build luajit gettext 
+sudo apt install \
+  libncurses5-dev \
+  libncursesw5-dev \
+  make \
+  cmake \
+  ninja-build \
+  luajit \
+  gettext \
+  libssl-dev \
+  libdbus-1-dev \
+  libsqlite3-dev
