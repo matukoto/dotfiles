2a371057fe44121299ae073855fbf1b98d301c7e
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-10-02 13:00:58 +0900

    feat: wezterm windows terminal: use nu shell

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -43,7 +43,7 @@ local config = {
 local mux = wezterm.mux
 
 wezterm.on('gui-startup', function()
-  local _, _, window = mux.spawn_window({ args = { 'pwsh.exe', '-NoLogo' } })
+  local _, _, window = mux.spawn_window({ args = { 'nu.exe', '-NoLogo' } })
   window:spawn_tab({ args = { 'wsl.exe', '--cd', '~' } })
 end)
 
