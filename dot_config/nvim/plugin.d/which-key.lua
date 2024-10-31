536860368f651ba79fcd411bbf9c2f8bb1f25ede
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-10-31 23:28:40 +0900

    feat(which-key): add-key

--- dot_config/nvim/plugin.d/which-key.lua
+++ dot_config/nvim/plugin.d/which-key.lua
@@ -1,1 +1,38 @@
 require('which-key').setup({})
+
+-- キーマップの登録
+local wk = require('which-key')
+
+wk.add({
+  g = {
+    name = 'LSP',
+    d = { '<cmd>lua vim.lsp.buf.definition()<CR>', 'Go to Definition' },
+    D = { '<cmd>vsplit lua vim.lsp.buf.definition()<CR>', 'Go to Definition in Split' },
+    l = { vim.lsp.buf.implementation, 'Go to Implementation' },
+    L = { '<cmd>Lspsaga peek_definition<CR>', 'Peek Definition' },
+    t = { '<cmd>Lspsaga peek_type_definition<CR>', 'Peek Type Definition' },
+    f = { '<cmd>Lspsaga finder ref<CR>', 'Find References' },
+    r = { '<cmd>Lspsaga rename<CR>', 'Rename' },
+    a = { '<cmd>Lspsaga code_action<CR>', 'Code Action' },
+    b = { '<cmd>Lspsaga show_buf_diagnostics<CR>', 'Show Buffer Diagnostics' },
+    w = { '<cmd>Lspsaga show_workspace_diagnostics<CR>', 'Show Workspace Diagnostics' },
+    e = { vim.diagnostic.open_float, 'Open Diagnostic Float' },
+    q = { vim.diagnostic.setqflist, 'Set Quickfix List' },
+    ['['] = { '<cmd>Lspsaga diagnostic_jump_next<CR>', 'Next Diagnostic' },
+    [']'] = { '<cmd>Lspsaga diagnostic_jump_prev<CR>', 'Previous Diagnostic' },
+  },
+}, { prefix = 'g' })
+
+wk.add({
+  ['<C-g>'] = {
+    name = 'Gin',
+    s = 'GinStatus',
+    a = 'Gin add --all',
+    c = 'Gin commit --quiet',
+    P = 'GinPatch ++opener=tabnew ++no-head %',
+    p = 'Gin push --quiet',
+    l = 'GinLog ++opener=tabnew',
+    b = 'GinBranch --all',
+    d = 'GinDiff',
+  },
+}, { prefix = '<C-g>' })
