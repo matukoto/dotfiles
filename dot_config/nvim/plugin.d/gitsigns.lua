a39234eff7afb32299b56c2559cd549ca579feee
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-11-16 15:38:15 +0900

    fix(gitsigns): キーマップを変更

--- dot_config/nvim/plugin.d/gitsigns.lua
+++ dot_config/nvim/plugin.d/gitsigns.lua
@@ -16,14 +16,9 @@ require('gitsigns').setup({
       vim.keymap.set(mode, l, r, opts)
     end
     -- Actions
-    -- map('n', '<leader>hs', gitsigns.stage_hunk)
-    -- map('n', '<leader>hr', gitsigns.reset_hunk)
-    map('v', '<leader>hs', function()
-      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-    end)
-    map('v', '<leader>hr', function()
-      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-    end)
+    map('n', '<C-g><', gitsigns.stage_hunk)
+    map('n', '<C-g>>', gitsigns.undo_stage_hunk)
+
     -- map('n', '<leader>hS', gitsigns.stage_buffer)
     -- map('n', '<leader>hu', gitsigns.undo_stage_hunk)
     -- map('n', '<leader>hR', gitsigns.reset_buffer)
