e7f2f4c1d4653b7aa5d88447702925079996fc3a
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2025-02-02 14:37:39 +0900

    feat(plugin): remove hawtkeys and incline

--- dot_config/nvim/rc/hawtkeys.lua
+++
@@ -1,9 +0,0 @@
--- require('hawtkeys').setup({
---   colors = {
---     normal = '#f5c359',
---     insert = '#78ccc5',
---     visual = '#9745be',
---     replace = '#c75c6a',
---     command = '#c75c6a',
---   },
--- })
