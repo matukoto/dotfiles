d12609d00bcc3b34373f2b5ac806b8cf527ed61f
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2025-04-06 15:12:45 +0900

    feat(snacks): add dashboard

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -21,13 +21,90 @@ return {
       auto_insert = true,
     },
     dashboard = {
+      preset = {
+        keys = {
+          {
+            icon = ' ',
+            key = 'f',
+            desc = 'Files',
+            action = ":lua Snacks.dashboard.pick('files')",
+          },
+          {
+            icon = ' ',
+            key = 'p',
+            desc = 'project',
+            action = ":lua Snacks.dashboard.pick('projects')",
+          },
+          { icon = ' ', key = 'i', desc = 'edit', action = ':ene | startinsert' },
+          {
+            icon = ' ',
+            key = 'g',
+            desc = 'grep',
+            action = ":lua Snacks.dashboard.pick('live_grep')",
+          },
+          {
+            icon = ' ',
+            key = 'r',
+            desc = 'Recent',
+            action = ":lua Snacks.dashboard.pick('oldfiles')",
+          },
+          {
+            icon = ' ',
+            key = 'c',
+            desc = 'Config',
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+          {
+            icon = '󰒲 ',
+            key = 'L',
+            desc = 'Lazy',
+            action = ':Lazy',
+            enabled = package.loaded.lazy ~= nil,
+          },
+          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+        },
+      },
       sections = {
-        { section = 'header' },
-        { section = 'keys', gap = 1, padding = 1 },
+        --{ section = 'header' },
+        { section = 'keys', padding = 1 },
+        {
+          pane = 1,
+          icon = ' ',
+          title = 'Recent Files',
+          section = 'recent_files',
+          indent = 2,
+          padding = 1,
+        },
+        {
+          pane = 2,
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = 'git status --short --branch --renames',
+          height = 7,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        {
+          pane = 2,
+          icon = ' ',
+          title = 'Projects',
+          section = 'projects',
+          indent = 2,
+          padding = 1,
+        },
         { section = 'startup' },
       },
     },
     picker = {
+      sources = {
+        sort = { fields = { 'idx', 'score:desc' } },
+      },
       formatters = {
         file = {
           filename_first = true,
