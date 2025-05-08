2b9fea0cf764a9568e15cb2d8ee499efedd1a5e7
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2025-05-08 09:51:11 +0900

    feat(project): change project root detection method

--- dot_config/nvim/lua/plugins/project.lua
+++ dot_config/nvim/lua/plugins/project.lua
@@ -1,4 +1,3 @@
--- dot_config/nvim/lua/plugins/project.lua
 return {
   'ahmedkhalf/project.nvim',
   -- Load early to detect project root on startup/buffer enter
@@ -13,7 +12,7 @@ return {
     detection_methods = { 'lsp', 'pattern' },
 
     -- Patterns used for "pattern" detection method
-    patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json' },
+    patterns = { 'package.json', '.git' },
 
     -- LSP clients to ignore when detecting project root
     ignore_lsp = {},
@@ -27,7 +26,7 @@ return {
     silent_chdir = true,
 
     -- Scope for changing directory: 'global', 'tab', 'win'
-    scope_chdir = 'global',
+    scope_chdir = 'tab',
 
     datapath = vim.fn.stdpath('data') .. '/project_nvim', -- Ensure subdirectory exists
   },
