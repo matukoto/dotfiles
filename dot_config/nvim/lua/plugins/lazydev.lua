06799dc1ccd9aeb1f07dc0030d9b341f216d60be
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2025-05-10 21:05:00 +0900

    feat(lazydev): add support for more libraries

--- dot_config/nvim/lua/plugins/lazydev.lua
+++ dot_config/nvim/lua/plugins/lazydev.lua
@@ -1,10 +1,15 @@
 return {
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        { path = 'snacks.nvim/lua/snacks' },
+  'folke/lazydev.nvim',
+  ft = 'lua',
+  opts = {
+    library = {
+      {
+        path = 'snacks.nvim/lua/snacks',
+        'lazy.nvim',
+        'luvit-meta/library',
+        'plenary.nvim',
+        '${3rd}/busted/library',
+        '${3rd}/luassert/library',
       },
     },
   },
