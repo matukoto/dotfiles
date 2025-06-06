91619c77d3e54cc9772ba4695573917eea609eac
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2025-06-06 14:12:11 +0900

    feat(render-markdown): add filetype event

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -3,6 +3,7 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   event = 'BufReadPre',
+  ft = 'markdown',
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {
