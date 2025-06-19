9bf1fcacd3c1438fe7f5ddf9ef96c24310a45162
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2025-06-19 22:37:26 +0900

    feat(plugin): add claudecode.nvim plugin

---
+++ dot_config/nvim/lua/plugins/claudecode.lua
@@ -0,0 +1,30 @@
+return {
+  'coder/claudecode.nvim',
+  event = 'VeryLazy',
+  cond = function()
+    local plugin = os.getenv('PRIVATE_PLUGIN_ENABLED')
+    if plugin ~= nil then
+      return true
+    else
+      return false
+    end
+  end,
+  config = true,
+  keys = {
+    { '<leader>a', nil, desc = 'AI/Claude Code' },
+    { '<leader>ac', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
+    { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
+    { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude' },
+    { '<leader>aC', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude' },
+    { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'v', desc = 'Send to Claude' },
+    {
+      '<leader>as',
+      '<cmd>ClaudeCodeTreeAdd<cr>',
+      desc = 'Add file',
+      ft = { 'NvimTree', 'neo-tree', 'oil' },
+    },
+    -- Diff management
+    { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
+    { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny diff' },
+  },
+}
