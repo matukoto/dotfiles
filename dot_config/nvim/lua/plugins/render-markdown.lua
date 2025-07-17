78d7943fb62bdea9607938f7d22c7ddfa5247331
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2025-07-17 10:18:39 +0900

    fix(render-markdown): blink.cmp completions don't work in markdown

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -6,6 +6,6 @@ return {
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {
-    completions = { blink = { enabled = true } },
+    -- completions = { blink = { enabled = true } },
   },
 }
