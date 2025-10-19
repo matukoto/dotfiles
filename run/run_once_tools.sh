88db734076a32e7232f819398b484e7f9d87d253
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2025-10-19 18:10:22 +0900

    feat(chezmoi): 不要なインストールスクリプトを削除

--- run/run_once_tools.sh
+++ run/run_once_tools.sh
@@ -3,30 +3,17 @@
 # run_once_hogehoge は `chezmoi apply` した際に以前に実行されたことがなければ実行されるファイル
 # 主にツールのインストールなど、一度だけ実行すれば良いものを記述する
 
-# rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# chezmoi
-sh -c "$(curl -fsLS get.chezmoi.io)"
-
 # aqua
 curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
 echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  aqua-installer" | sha256sum -c
 chmod +x aqua-installer
 ./aqua-installer
 
-# mise
-# curl https://mise.run | sh
-mise i
-
-# atuin
-# bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
-
 # nu starship
-git clone https://github.com/nushell/nushell.git
-cd nushell || exit
-cargo build --release --workspace
-cargo run --release
+# git clone https://github.com/nushell/nushell.git
+# cd nushell || exit
+# cargo build --release --workspace
+# cargo run --release
 
 curl -sS https://starship.rs/install.sh | sh
 mkdir ~/.cache/starship
