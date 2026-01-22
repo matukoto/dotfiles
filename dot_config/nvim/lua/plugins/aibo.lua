dc4550f054d4968a7b8e7df1527cb688e0aeb911
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2026-01-22 18:32:49 +0900

    feat(aibo): 最低限のキーマップを追加

--- dot_config/nvim/lua/plugins/aibo.lua
+++ dot_config/nvim/lua/plugins/aibo.lua
@@ -1,7 +1,17 @@
 return {
   'lambdalisue/nvim-aibo',
-  -- Optional: setup can be omitted for default configuration
   config = function()
-    require('aibo').setup()
+    require('aibo').setup({
+      controller = {
+        mappings = function(bufnr)
+          local opts = { buffer = bufnr, nowait = true, silent = true }
+          vim.keymap.set('i', '<C-Enter>', '<Plug>(aibo-submit)', opts)
+        end,
+      },
+    })
+
+    vim.keymap.set('n', '<leader>at', ':Aibo -opener=tabnew gemini<CR>', { desc = 'Aibo Gemini in Tab' })
+    --選択範囲を Aibo に送信
+    vim.keymap.set('v', '<leader>as', ':AiboSend -submit<CR>', { desc = 'Send selection to Aibo' })
   end,
 }
