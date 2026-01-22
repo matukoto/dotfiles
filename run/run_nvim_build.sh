da26f1de47270baa4134cc8a162a9a81a6da0e85
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2026-01-22 18:49:41 +0900

    feat(build): neovim のビルドスクリプトを修正

--- run/run_nvim_build.sh
+++ run/run_nvim_build.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-CURRENT_DIR=$(pwd "$0")
+# 1. 絶対パスでNeovimのディレクトリを指定
+NVIM_DIR="${HOME}/work/github.com/neovim/neovim"
 
-cd "${HOME}"/work/github.com/neovim/neovim || exit
+# ディレクトリが存在するかチェック
+if [ ! -d "$NVIM_DIR" ]; then
+  echo "Directory $NVIM_DIR does not exist."
+  exit 1
+fi
 
+cd "$NVIM_DIR" || exit
+
+# 2. 更新処理
 git fetch --prune origin
 git rebase origin/master
 
+# 3. クリーンアップ（確実に消去）
 make distclean
+rm -rf .deps build
 
+# 4. ビルド
+# BUNDLED_CMAKE_FLAG は CMAKE_EXTRA_FLAGS 内で渡すのが一般的です
 make CMAKE_BUILD_TYPE=Release \
-  CMAKE_INSTALL_PREFIX="$HOME"/.local/ \
-  BUNDLED_CMAKE_FLAG='-DUSE_BUNDLED_TS_PARSERS=OFF'
+  CMAKE_INSTALL_PREFIX="$HOME/.local" \
+  CMAKE_EXTRA_FLAGS="-DUSE_BUNDLED_TS_PARSERS=OFF"
 
+# 5. インストール
 make install
-
-cd "${CURRENT_DIR}" || exit
