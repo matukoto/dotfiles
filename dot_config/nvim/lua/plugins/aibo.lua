8eb68fa3ce6fe47736e855ae6e4028748d1b19fa
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2026-02-10 18:18:50 +0900

    feat(aibo): keybind を追加

--- dot_config/nvim/lua/plugins/aibo.lua
+++ dot_config/nvim/lua/plugins/aibo.lua
@@ -1,17 +1,24 @@
 return {
   'lambdalisue/nvim-aibo',
-  config = function()
-    require('aibo').setup({
-      controller = {
-        mappings = function(bufnr)
-          local opts = { buffer = bufnr, nowait = true, silent = true }
-          vim.keymap.set('i', '<C-Enter>', '<Plug>(aibo-submit)', opts)
-        end,
-      },
-    })
-
-    vim.keymap.set('n', '<leader>at', ':Aibo -opener=tabnew gemini<CR>', { desc = 'Aibo Gemini in Tab' })
-    --選択範囲を Aibo に送信
-    vim.keymap.set('v', '<leader>as', ':AiboSend -submit<CR>', { desc = 'Send selection to Aibo' })
-  end,
+  opts = {
+    console = {
+      mappings = function(bufnr)
+        vim.keymap.set({ 'n', 'i' }, '<CR>', '<Plug>(aibo-submit)', { buffer = bufnr, desc = 'Submit to Aibo' })
+      end,
+    },
+    prompt = {
+      on_attach = function(bufnr)
+        vim.keymap.set({ 'n', 'i' }, '<Tab>', '<Plug>(aibo-send)<Tab>', { buffer = bufnr, desc = 'Send Tab to Aibo' })
+        vim.keymap.set({ 'n', 'i' }, '<C-n>', '<Plug>(aibo-send)<Down>', { buffer = bufnr, desc = 'Send Down to Aibo' })
+        vim.keymap.set({ 'n', 'i' }, '<C-p>', '<Plug>(aibo-send)<Up>', { buffer = bufnr, desc = 'Send Up to Aibo' })
+        vim.keymap.set({ 'n', 'i' }, '<C-c>', '<Plug>(aibo-send)<Esc>', { buffer = bufnr, desc = 'Send Up to Aibo' })
+      end,
+    },
+    keys = {
+      vim.keymap.set('n', '<leader>at', ':Aibo -opener=tabnew opencode --continue<CR>', { desc = 'Aibo opencode in Tab' }),
+      vim.keymap.set('n', '<leader>av', ':Aibo -opener=vsplit opencode --continue<CR>', { desc = 'Aibo opencode in Vsplit' }),
+      --選択範囲を Aibo に送信
+      vim.keymap.set('v', '<leader>as', ':AiboSend -submit<CR>', { desc = 'Send selection to Aibo' }),
+    },
+  },
 }
