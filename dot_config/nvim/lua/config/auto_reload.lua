94aa1224ab6ff456c666c3f6d4676ddde9bd79f7
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2026-02-20 19:37:23 +0900

    fix(auto_reload): auto_reload が動作するように修正 lazy spec 形式で書いていたのを、通常の Lua モジュール形式に変更した

--- dot_config/nvim/lua/config/auto_reload.lua
+++ dot_config/nvim/lua/config/auto_reload.lua
@@ -1,65 +1,66 @@
-return {
-  {
-    event = 'VeryLazy',
-    config = function()
-      local auto_reload_group = vim.api.nvim_create_augroup('AutoReloadOnExternalChange', { clear = true })
+local M = {}
 
-      local function checktime_if_safe()
-        if vim.fn.mode() ~= 'c' then
-          vim.cmd('silent! checktime')
+function M.setup()
+  local auto_reload_group = vim.api.nvim_create_augroup('AutoReloadOnExternalChange', { clear = true })
+
+  local function checktime_if_safe()
+    if vim.fn.mode() ~= 'c' then
+      vim.cmd('silent! checktime')
+    end
+  end
+
+  vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+    pattern = '*',
+    group = auto_reload_group,
+    callback = checktime_if_safe,
+  })
+
+  local idle_timer = vim.uv.new_timer()
+
+  local function stop_idle_timer()
+    if idle_timer and idle_timer:is_active() then
+      idle_timer:stop()
+    end
+  end
+
+  local function start_idle_timer()
+    stop_idle_timer()
+    idle_timer:start(
+      1000,
+      1000,
+      vim.schedule_wrap(function()
+        if vim.fn.mode() ~= 'c' and vim.api.nvim_get_mode().blocking == false then
+          vim.api.nvim_exec_autocmds('User', { pattern = 'IdleTick' })
         end
-      end
+      end)
+    )
+  end
 
-      vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
-        pattern = '*',
-        group = auto_reload_group,
-        callback = checktime_if_safe,
-      })
+  vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    pattern = '*',
+    group = auto_reload_group,
+    callback = start_idle_timer,
+  })
 
-      local idle_timer = vim.uv.new_timer()
+  vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'InsertEnter', 'InsertLeave', 'CmdlineEnter' }, {
+    pattern = '*',
+    group = auto_reload_group,
+    callback = stop_idle_timer,
+  })
 
-      local function stop_idle_timer()
-        if idle_timer and idle_timer:is_active() then
-          idle_timer:stop()
-        end
-      end
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'IdleTick',
+    group = auto_reload_group,
+    callback = checktime_if_safe,
+  })
 
-      local function start_idle_timer()
-        stop_idle_timer()
-        idle_timer:start(
-          1000,
-          1000,
-          vim.schedule_wrap(function()
-            if vim.fn.mode() ~= 'c' and vim.api.nvim_get_mode().blocking == false then
-              vim.api.nvim_exec_autocmds('User', { pattern = 'IdleTick' })
-            end
-          end)
-        )
-      end
+  vim.api.nvim_create_autocmd('VimLeavePre', {
+    pattern = '*',
+    group = auto_reload_group,
+    callback = stop_idle_timer,
+  })
+end
 
-      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        pattern = '*',
-        group = auto_reload_group,
-        callback = start_idle_timer,
-      })
+M.setup()
 
-      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'InsertEnter', 'InsertLeave', 'CmdlineEnter' }, {
-        pattern = '*',
-        group = auto_reload_group,
-        callback = stop_idle_timer,
-      })
-
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'IdleTick',
-        group = auto_reload_group,
-        callback = checktime_if_safe,
-      })
-
-      vim.api.nvim_create_autocmd('VimLeavePre', {
-        pattern = '*',
-        group = auto_reload_group,
-        callback = stop_idle_timer,
-      })
-    end,
-  },
-}
+return M
