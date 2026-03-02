613797396f4a24443a91623efa410885983befdf
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2026-03-02 16:07:56 +0900

    feat(nix): add nix lsp and formatter
    - nixd
    - nixfmt-rfc-style

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -21,6 +21,8 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    pkgs.nixfmt-rfc-style
+    pkgs.nixd
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
