d4870974b5d04960111e730f6a28fdc994780f70
MATSUMOTO Keisuke <matukotoo@gmail.com>
2026-04-03 16:11:22 +0900

    chore(blink): スニペットのタブキーバインドを無効化

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -23,8 +23,8 @@ return {
         -- ['<Down>'] = { 'select_next', 'fallback' },
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-        ['<Tab>'] = { 'snippet_forward', 'fallback' },
-        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        -- ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        -- ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
         ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
         ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' }, -- Example: Select previous
         ['<C-n>'] = { 'select_next', 'fallback_to_mappings' }, -- Example: Select previous
