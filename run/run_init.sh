c979b2bea6627059dff32f978c54f16514f586c2
MATSUMOTO Keisuke <matukotoo@gmail.com>
2026-04-15 16:17:35 +0900

    chore(run): run ディレクトリの削除

--- run/run_init.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-mise i
-
-aqua i -a
-
-ghq get https://github.com/vim/vim.git
-ghq get https://github.com/neovim/neovim.git
--- run/run_windows.sh
+++
@@ -1,7 +0,0 @@
-scoop bucket add extras
-scoop install git
-scoop install gh
-scoop insstall everything
-
-
-winget install fancywm
