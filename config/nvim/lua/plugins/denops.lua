c7587f47526b47247531d68a6bb82b87682255b4
MATSUMOTO Keisuke <matukotoo@gmail.com>
2026-08-21 22:52:11 +0900

    feat(denops): allow-all -> 最低限の権限に変更

--- config/nvim/lua/plugins/denops.lua
+++ config/nvim/lua/plugins/denops.lua
@@ -9,14 +9,19 @@ return {
     -- Set Deno arguments for the Denops server
     vim.g['denops#server#deno_args'] = {
       '-q', -- Quiet mode
-      '--no-lock', -- Disable lock file
-      '-A', -- Allow all permissions
+      -- '--no-lock', -- Disable lock file
+      -- '-A', -- Allow all permissions
       '--unstable-ffi', -- Enable unstable FFI API
       '--unstable-kv', -- Enable unstable KV store API
+      '--allow-read',
+      '--allow-env',
+      '--allow-net=127.0.0.1,cdn.jsdelivr.net',
+      '--allow-write',
+      '--allow-run=git',
     }
 
     -- Optional: Debug mode (uncomment if needed)
-    -- vim.g['denops#debug'] = 1
+    vim.g['denops#debug'] = 1
 
     -- Optional: Shared server address (uncomment if using denops-shared-server)
     -- vim.g.denops_server_addr = "127.0.0.1:32123"
